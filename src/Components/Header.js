e26fd97c675b2d5968fad6fa5030bc73cb640258
James-Dodd <jamesfenndodd@gmail.com>
2022-03-21 17:00:52 +0000

    added changes required

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,23 +1,34 @@
 import React from "react";
-import Button from '@mui/material/Button';
-import {useNavigate} from 'react-router-dom';
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 
-const Header=({search})=>{
-    const navigate= useNavigate();
-    const onClickHandler=()=>{
-        if (search===true){
-            navigate("/")
-        }else{
-            navigate("/search")
-        }
+const Header = ({ search }) => {
+  const navigate = useNavigate();
+  const onClickHandler = () => {
+    if (search === true) {
+      navigate("/");
+    } else {
+      navigate("/search");
     }
+  };
 
-    return (
-        <div class="list-books-title">
-              <h1>MyReads</h1>
-              <Button onClick={()=>onClickHandler()} style={{position:"absolute", right:"100px", top:"20px", backgroundColor:"purple", color:'whitesmoke'}}>{(search===true?"home":"Search")}</Button>
-        </div>
-    )
+  return (
+    <div class="list-books-title">
+      <h1>MyReads</h1>
+      <Button
+        onClick={() => onClickHandler()}
+        style={{
+          position: "absolute",
+          right: "100px",
+          top: "20px",
+          backgroundColor: "purple",
+          color: "whitesmoke",
+        }}
+      >
+        {search === true ? "home" : "Search"}
+      </Button>
+    </div>
+  );
 };
 
 export default Header;
--- src/SerchPage.js
+++ src/SerchPage.js
@@ -1,61 +1,94 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import Header from "./Components/Header";
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 import BookCard from "./Components/BookCard";
-import Grid from '@mui/material/Grid';
-import Divider from '@mui/material/Divider';
-import * as BooksApi from './BooksAPI';
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+import DangerousIcon from "@mui/icons-material/Dangerous";
+import * as BooksApi from "./BooksAPI";
 
+const App = (props) => {
+  const { bookstate, trigger } = props;
+  const bookArr = Object.values(bookstate);
+  const [booksres, setBooksres] = useState([]);
+  const gridContainer = {
+    display: "grid",
+    gridTemplateColumns: "repeat(5, 1fr)",
+    paddingTop: "10px",
+  };
+  const debounce = (func, wait) => {
+    let timeout;
 
-const App =()=>{
-    const [booksres, setBooksres]=useState({});
-    const gridContainer = {
-        display: "grid",
-        gridTemplateColumns: "repeat(5, 1fr)",
-        paddingTop: '10px',
+    return function executedFunction(...args) {
+      const later = () => {
+        clearTimeout(timeout);
+        func(...args);
       };
-    const queryErr=(value)=>{
-        console.log('here is value --->', value)
-        if (value===undefined){
-            console.log('triggered');
-            return 'a';
-        }
-        return value;
-    }
-    const changeHandler=(e)=>{
-        console.log(e.target.value)
-        
-        BooksApi.search(queryErr(e.target.value)).then((books) => {
-            const booksDict = {};
-            for (const book of books) {
-              booksDict[book.id] = book;
-            }
-            setBooksres(booksDict)
-            console.log('here is books --->', booksres)
-          });
-    }
-    return (
-        <div>
-            <div>
-                <Header search={true}/>
-                <Box
-                    component="form"
-                    noValidate
-                    autoComplete="off"
-                    >
-                        <TextField onKeyDown={changeHandler} style={{minWidth:'1000px', position:'absolute', left:'450px', marginTop:'20px'}}id="Search" label="Search" variant="outlined" />
-                </Box>
-                <Box style={{marginTop:'100px'}} sx={{ flexGrow: 1 }}>
-                <Divider/>
-                <Grid sx={gridContainer} container columns={3} justifyContent="center" spacing={2}>
-                    {Object.entries(booksres).map(([key, value]) => <Grid item xs={6} md={8}> <BookCard book={value}/> </Grid>)}
-                    </Grid>
-                </Box>
-            </div>
 
-        </div>
-    )
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    };
+  };
+  const queryErr = (value) => {
+    if (value === undefined) {
+      return [];
+    }
+    return value;
+  };
+  const processChange = debounce(function changeHandler(e) {
+    e.target.value !== ""
+      ? BooksApi.search(e.target.value).then((books) => {
+          const booksDict = Object.values(books);
+          const booksHold = booksDict.map(
+            (obj) => bookArr.find((o) => o.id === obj.id) || obj
+          );
+          setBooksres(booksHold);
+        })
+      : setBooksres([]);
+  }, 250);
+  return (
+    <div>
+      <div>
+        <Header search={true} />
+        <Box component="form" noValidate autoComplete="off">
+          <TextField
+            onKeyUp={processChange}
+            style={{
+              minWidth: "1000px",
+              position: "absolute",
+              left: "450px",
+              marginTop: "20px",
+            }}
+            id="Search"
+            label="Search"
+            variant="outlined"
+          />
+        </Box>
+        <Box style={{ marginTop: "100px" }} sx={{ flexGrow: 1 }}>
+          <Divider />
+          <Grid
+            sx={gridContainer}
+            container
+            columns={3}
+            justifyContent="center"
+            spacing={2}
+          >
+            {booksres === undefined ? (
+              <DangerousIcon fontSize="large" color="disabled" />
+            ) : (
+              booksres.map((book) => (
+                <Grid item xs={6} md={8}>
+                  {" "}
+                  <BookCard book={book} getBook={trigger} />{" "}
+                </Grid>
+              ))
+            )}
+          </Grid>
+        </Box>
+      </div>
+    </div>
+  );
 };
 
 export default App;
